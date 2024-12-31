dc0dc16f724dfcf99b566b24acaf027545fe3128
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-31 10:36:39 +0530

    Add files via upload

---
+++ profit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int t=S.nextInt();
+		while(t-->0)
+		{
+		    int x=S.nextInt();
+		    int y=S.nextInt();
+		    int z=x/10;
+		    System.out.println(z+y);
+		}
+
+	}
+}
